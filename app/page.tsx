5821850ba6e7a5bcad1aace46d097fa16d345ba4
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-11-15 16:33:28 +0530

    Update background color and add chess.js dependency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+"use client";
+import { Chess } from "chess.js";
+import React, { useMemo, useState } from "react";
+import { Chessboard } from "react-chessboard";
 
-export default function Home() {
+const buttonStyle = {
+  padding: "10px 20px",
+  margin: "10px",
+  border: "none",
+  color: "white",
+  fontSize: "16px",
+  cursor: "pointer",
+};
+
+const boardWrapper = {
+  height: "100vh",
+  backgroundColor: "#f0d9b5",
+};
+class Engine {
+  stockfish: Worker;
+  onMessage: (callback: (e: any) => void) => void;
+  // sendMessage: (message: any) => void;
+
+  constructor() {
+    this.stockfish = new Worker("./stockfish.js");
+    this.onMessage = (callback) => {
+      this.stockfish.addEventListener("message", (e) => {
+        const bestMove = e.data?.match(/bestmove\s+(\S+)/)?.[1];
+        callback({ bestMove });
+      });
+    };
+    // Init engine
+    this.onMessage(() => "uci");
+    this.onMessage(() => "isready");
+  }
+  sendMessage(arg0: string) {
+    throw new Error("Method not implemented.");
+  }
+
+  evaluatePosition(fen: any, depth: any) {
+    this.stockfish.postMessage(`position fen ${fen}`);
+    this.stockfish.postMessage(`go depth ${depth}`);
+  }
+  stop() {
+    this.onMessage(() => "stop"); // Run when changing positions
+  }
+  quit() {
+    this.onMessage(() => "quit"); // Good to run this before unmounting.
+  }
+}
+
+const HomePage = () => {
+  const levels = {
+    "Easy 🤓": 2,
+    "Medium 🧐": 8,
+    "Hard 😵": 18,
+  };
+  const engine = useMemo(() => new Engine(), []);
+  const game = useMemo(() => new Chess(), []);
+
+  const [gamePosition, setGamePosition] = useState(game.fen());
+  const [stockfishLevel, setStockfishLevel] = useState(2);
+
+  function findBestMove() {
+    engine.evaluatePosition(game.fen(), stockfishLevel);
+
+    engine.onMessage(({ bestMove }: { bestMove: string }) => {
+      if (bestMove) {
+        // In latest chess.js versions you can just write ```game.move(bestMove)```
+        // game.move({
+        //   from: bestMove.substring(0, 2),
+        //   to: bestMove.substring(2, 4),
+        //   promotion: bestMove.substring(4, 5),
+        // });
+        game.move(bestMove);
+        console.log("bestMove", bestMove);
+        setGamePosition(game.fen());
+      }
+    });
+  }
+
+  function onDrop(sourceSquare: any, targetSquare: any, piece: any) {
+    const move = game.move({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: piece[1].toLowerCase() ?? "q",
+    });
+    setGamePosition(game.fen());
+
+    // illegal move
+    if (move === null) return false;
+
+    // exit if the game is over
+    if (game.isGameOver() || game.isDraw()) return false;
+
+    findBestMove();
+
+    return true;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div style={boardWrapper} className="flex flex-col items-center">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        {Object.entries(levels).map(([level, depth]) => (
+          <button
+            key={depth}
+            style={{
+              ...buttonStyle,
+              backgroundColor: depth === stockfishLevel ? "#B58863" : "#f0d9b5",
+            }}
+            onClick={() => setStockfishLevel(depth)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {level}
+          </button>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="w-2/3">
+        <Chessboard
+          id="PlayVsStockfish"
+          position={gamePosition}
+          onPieceDrop={onDrop}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          style={{ ...buttonStyle, backgroundColor: "#B58863" }}
+          onClick={() => {
+            game.reset();
+            setGamePosition(game.fen());
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          New game
+        </button>
+        <button
+          style={{ ...buttonStyle, backgroundColor: "#B58863" }}
+          onClick={() => {
+            game.undo();
+            setGamePosition(game.fen());
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Undo
+        </button>
       </div>
-    </main>
-  )
-}
+    </div>
+  );
+};
+
+export default HomePage;
