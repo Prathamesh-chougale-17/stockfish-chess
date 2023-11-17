fcad67fc7f2f71dab6fc3266aaa237c1cf145c44
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-11-17 17:48:46 +0530

    Update Worker interface and import statement

--- app/Worker.ts
+++ app/Worker.ts
@@ -1,16 +1,25 @@
-// at eval (./app/page.tsx:35:71)
-//     at HomePage (./app/page.tsx:35:66)
-//    5 |   // sendMessage: (message: any) => void;
-//    6 |   constructor() {
-// >  7 |     this.stockfish = new Worker("./stockfish.js");
-//      |                         ^
-//    8 |     this.onMessage = (callback) => {
-//    9 |       this.stockfish.addEventListener("message", (e) => {
-//   10 |         const bestMove = e.data?.match(/bestmove\s+(\S+)/)?.[1];
+export interface Worker extends EventTarget, AbstractWorker {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Worker/message_event) */
+    onmessage: ((this: Worker, ev: MessageEvent) => any) | null;
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Worker/messageerror_event) */
+    onmessageerror: ((this: Worker, ev: MessageEvent) => any) | null;
+    /**
+     * Clones message and transmits it to worker's global environment. transfer can be passed as a list of objects that are to be transferred rather than cloned.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Worker/postMessage)
+     */
+    postMessage(message: any, transfer: Transferable[]): void;
+    postMessage(message: any, options?: StructuredSerializeOptions): void;
+    /**
+     * Aborts worker's associated global environment.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Worker/terminate)
+     */
+    terminate(): void;
+    addEventListener<K extends keyof WorkerEventMap>(type: K, listener: (this: Worker, ev: WorkerEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener<K extends keyof WorkerEventMap>(type: K, listener: (this: Worker, ev: WorkerEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+    // constructor(scriptURL: string | URL, options?: WorkerOptions | undefined): Worker;
+}
 
-//with reference to above error write a code for worker.ts
-
-// Path: app/Worker.ts
-// import Worker from 'typescript/lib/lib.webworker'
-// typescript/lib/lib.webworker is not a module
-// 1 | // Path: app/Worker.ts
